bb95b6c2bba7a329a1701803e80f15102e0695c3
Allyson Cardozo <allyson.csharp@gmail.com>
2020-05-15 15:44:03 -0300

    Inclusão da classe genérica(abstrata) para os domínios

---
+++ implementacao/Prova/src/main/java/com/abstracts/ABase.java
@@ -0,0 +1,47 @@
+package com.abstracts;
+
+import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class ABase<T> implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Id;
+
+	public Integer getId() { return Id; }
+
+	public void setId(Integer id) { Id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ABase other = (ABase) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+	
+}
