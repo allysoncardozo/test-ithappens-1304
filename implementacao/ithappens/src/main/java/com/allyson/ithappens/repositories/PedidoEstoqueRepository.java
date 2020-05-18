20d9a0b4f96f1d36ffff7db090892788668047cf
Allyson Cardozo <allyson.csharp@gmail.com>
2020-05-17 21:06:10 -0300

    Projeto Atualizado!

--- implementacao/ithappens/src/main/java/com/allyson/ithappens/repositories/PedidoEstoqueRepository.java
+++ implementacao/ithappens/src/main/java/com/allyson/ithappens/repositories/PedidoEstoqueRepository.java
@@ -1,16 +1,11 @@
 package com.allyson.ithappens.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.allyson.ithappens.domain.ItensPedido;
 import com.allyson.ithappens.domain.PedidoEstoque;
 
 @Repository
 public interface PedidoEstoqueRepository extends JpaRepository<PedidoEstoque, Integer>{
-	@Query(value = "SELECT * FROM ITENS_PEDIDO where pedido_estoque_id = 1", nativeQuery = true)	
-	public List<ItensPedido> findItensByVendaId(Integer vendaId);
+	
 }
